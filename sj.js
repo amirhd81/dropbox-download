a9e095cf3783cde477d2d590c973e46e6037a042
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-04-30 22:44:27 +0330

    fds

--- sj.js
+++ sj.js
@@ -8,6 +8,7 @@ const browser = await chromium.launch({
     executablePath: chromiumPath,
     headless: true
    });
+    
 
     let clicked = false;
   const logFile = fs.createWriteStream('network_log.txt', { flags: 'a' });
@@ -31,8 +32,17 @@ const browser = await chromium.launch({
 
   await page.fill("#password", "gvc277");
 
+    console.log("password got filled")
+
   await page.click('button[type="submit"]');
+
     clicked = true
+    console.log(clicked, "buttong clicked")
+    const exists = await page.locator('p.invisible').count() > 0;
+
+console.log(exists, "error exists");
+    
+
 
   logFile.end();
   
